387f6c1c58b74b861064d3a2ab407a78b390ceff
danny galimi <dannygalimi13@aim.com>
2021-02-08 22:17:35 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,30 @@
+class Patient
+  attr_accessor :name, :appointment
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @appointment = appointment
+    @@all << self
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def new_appointment(date,doctor)
+    Appointment.new(date,self,doctor) 
+  end 
+  
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.appointment == self
+  end 
+end
+    
+  def doctors
+      
+  end 
+  
+end 
